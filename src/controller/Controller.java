0e7225c567c3f8c87d943d8b864aa6bc51ea6368
Letao <letaowang7@gmail.com>
2021-12-15 02:26:47 +0100

    Modify V1.3

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -29,7 +29,7 @@ public class Controller {
         for(Graph graph : listGraph){
             drawGraph(graph);
             do{
-                update(graph);
+                update(graph, true);
                 try {
                     Thread.sleep(200);
                 } catch (InterruptedException e) {
@@ -44,13 +44,25 @@ public class Controller {
         System.out.println(listOutput);
     }
 
-    public void update(Graph graph){
+    public void runAllWithOutGraphic(){
+        for(Graph graph : listGraph){
+            do{
+                update(graph, false);
+                if(!flag) break;
+            }while(path.get(0) != graph.getEndV());
+            listOutput.add(flag);
+            path.clear();
+        }
+        System.out.println(listOutput);
+    }
+
+    public void update(Graph graph, Boolean graphic){
         int ncols = graph.getNcols();
         int nlines = graph.getNlines();
         int startV = graph.getStartV();
         int endV = graph.getEndV();
 
-        prisonerMove(graph, startV, endV, ncols, board);
+        prisonerMove(graph, startV, endV, ncols, board, graphic);
 
     }
 
@@ -64,7 +76,7 @@ public class Controller {
         window.setVisible(true);
     }
 
-    public void prisonerMove(Graph graph, int start, int end, int ncols, Board board) {
+    public void prisonerMove(Graph graph, int start, int end, int ncols, Board board, Boolean graphic) {
         graph.getVertexlist().get(start).setTimeFromSource(0);
         //mettre tous les noeuds du graphe dans la liste des noeuds a visiter:
         HashSet<Integer> to_visit = new HashSet<Integer>();
@@ -106,11 +118,12 @@ public class Controller {
             }
         }
         if(newDistance > 400){
-            System.out.println("Not found!");
             flag = false;
         } else {
             path.addFirst(min_v);
-            board.addPath(graph, path);
+            if(graphic){
+                board.addPath(graph, path);
+            }
             flag = true;
         }
     }
--- src/controller/Execute.java
+++ src/controller/Execute.java
@@ -4,5 +4,6 @@ public class Execute {
     public static void main(String[] args) {
         Controller controller = new Controller();
         controller.runAllWithGraphic();
+//        controller.runAllWithOutGraphic();
     }
 }
